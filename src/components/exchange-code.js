fc2be8228c07740e5b89f9db4bb81583d855c717
horsemi <horsemi@outlook.com>
2020-02-14 16:29:43 +0800

    添加神秘代码

--- src/components/exchange-code.js
+++ src/components/exchange-code.js
@@ -1,11 +1,19 @@
 
 export const exchangeCode = [
   {
-    code: "12345",
+    code: "a62d0889404a9d5b4bb50368ba4861f56921af0a",
     useful: true,
   },
   {
-    code: "67890",
+    code: "df840e2e74f21b2d1ccfde63e5a897bf49816bfa",
     useful: true,
-  }
+  },
+  {
+    code: "6198D4EB0A7965E331AC037881879AC0EEFE4107",
+    useful: true,
+  },
+  {
+    code: "E8B3CC5C40516E301418B55D6A79E4A0D5171B00",
+    useful: true,
+  },
 ];
